89444a5a3137b6ab744f6c1d26bf0f9ac319b88b
vinicius tomaz stefaneli <vinicius.stefaneli@fatec.sp.gov.br>
2025-06-24 21:05:49 -0300

    First commit

---
+++ Cash_Register/src/main/java/br/com/vinicius/Cash_Register/CashRegisterApplication.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.Cash_Register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashRegisterApplication.class, args);
+	}
+
+}
